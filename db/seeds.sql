de302d380fb498d424ed689a547c966b322592a2
Allain Bernal <llnbrnl017@gmail.com>
2018-12-19 21:57:38 -0800

    Actually did things! Almost done

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Krabby PAtty');
+INSERT INTO burgers (name) VALUES ('Pretty Patty');
+INSERT INTO burgers (name, sleepy) VALUES ('Krabby Double Deluxe', true);
+INSERT INTO burgers (name, sleepy) VALUES ('Double Triple Bossy Deluxe', true);
+INSERT INTO burgers (name, sleepy) VALUES ('Triple Krabby Supreme', true);
+INSERT INTO burgers (name) VALUES ('Nature Patty');
